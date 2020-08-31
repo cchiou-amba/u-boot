9587d167090184919cc901a31f1dbcf37040d8a5
Jorney <qtu@ambarella.com>
2020-08-31 14:52:15 +0800

    CV25: add dts in source tree

--- build_cv25.sh
+++ build_cv25.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #make distclean O=/tmp/u-boot-cv25
 make ambarella_cv25_defconfig O=/tmp/u-boot-cv25/
-make -j 4 O=/tmp/u-boot-cv25/ EXT_DTB=/home/qtu/work/stable/ambarella/boards/cv25_hazelnut/cv25_hazelnut-multi.fit
+make -j 4 O=/tmp/u-boot-cv25/
+#EXT_DTB=/home/qtu/work/stable/ambarella/boards/cv25_hazelnut/cv25_hazelnut-multi.fit
 cp /tmp/u-boot-cv25/u-boot.bin ~/share/
 #aarch64-linux-gnu-objdump -D /tmp/u-boot/u-boot > /tmp/a

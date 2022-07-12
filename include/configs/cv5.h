752e4550cdc42037187c6073b65cfe9e5f32530d
Jian He <jianhe@ambarella.com>
2022-07-12 18:31:40 +0800

    add a pcie boot arg
    Signed-off-by: Jian He <jianhe@ambarella.com>
    Change-Id: I905b171f5d2ef8de12e23122b6825d9e440630e7

--- include/configs/cv5.h
+++ include/configs/cv5.h
@@ -54,8 +54,9 @@
 #define CONFIG_EXTRA_ENV_SETTINGS						\
 	"serial#=Ambarella CV5\0"						\
 	"bootargs_nand= ubi.mtd=rootfs rootfstype=ubifs rw root=ubi0:rootfs \0"	\
+	"pcie_arg= pci=nomsi,pcie_bus_perf pcie_pme=nomsi fw_devlink=permissive \0" \
 	"boot_nand=setenv bootargs "						\
-		"console=${console} ${bootargs_nand} ${mtdparts}; "		\
+		"console=${console} ${bootargs_nand} ${pcie_arg} ${mtdparts}; "		\
 		"nand read ${kernel_addr} kernel; "				\
 		"booti ${kernel_addr} - ${fdtaddr} \0"
 #endif

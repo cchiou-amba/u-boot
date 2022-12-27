6844b2c1e8b1325dd2b19ef45bd73a172f5cda20
Yupeng Chang <ypchang@ambarella.com>
2022-12-27 18:36:24 +0800

    u-boot: fix compile warnings complaining CONFIG_BOOTCOMMAND redefined
    Change-Id: Ib63ad4838d84b76ac42b4569ef98ad1679400adb

--- include/configs/cv5.h
+++ include/configs/cv5.h
@@ -108,6 +108,9 @@
 
 #endif
 
+#if defined(CONFIG_BOOTCOMMAND)
+#undef CONFIG_BOOTCOMMAND
+#endif
 #define CONFIG_BOOTCOMMAND							\
     "if test ${AmbaEnv_boot_mode} = nand;"          \
     "then "                                         \

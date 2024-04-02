1a472bb9d1df72f3c736b170386bb3e9fab39e09
Gordon <gjweng@ambarella.com>
2024-04-02 11:44:37 +0800

    U-boot: Fix Multi cluster memory layout break lychee os single cluster memory.
    Change-Id: I01556c2060ac59486dbaf90a5466891b0711f4e4

--- arch/arm/lib/bootm-fdt.c
+++ arch/arm/lib/bootm-fdt.c
@@ -52,14 +52,15 @@ int arch_fixup_fdt(void *blob)
 
 #if defined(CONFIG_AMBA_BOOT_SECONDARY_CORTEX)
 	const char *clusters_mem = "/memory";
+	const void *fdt = gd->fdt_blob;
 	const unsigned int *prop;
 	int offset;
 
-	offset = fdt_path_offset(blob, clusters_mem);
+	offset = fdt_path_offset(fdt, clusters_mem);
 	if (offset < 0)
 		goto exit;
 
-	prop = fdt_getprop(blob, offset, "reg", NULL);
+	prop = fdt_getprop(fdt, offset, "reg", NULL);
 	if (prop) {
 		start[0] = ((unsigned long)fdt32_to_cpu(prop[0]) << 32) | fdt32_to_cpu(prop[1]);
 		size[0] = ((unsigned long)fdt32_to_cpu(prop[2]) << 32) | fdt32_to_cpu(prop[3]);

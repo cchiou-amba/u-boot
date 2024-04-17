84ebecc35725e14809a6700ac7806bc90a0a4be0
yyua <yyua@ambarella.com>
2024-04-17 17:20:58 +0800

    modify Image and dtb path for slave clusters in u-boot env
    Change-Id: Ia4b89ae73f0d5f7954813bc16a1f1f909a3d1af9

--- include/configs/n1_cooper_max.h
+++ include/configs/n1_cooper_max.h
@@ -48,12 +48,12 @@
 
 #ifdef CONFIG_AMBA_BOOT_SECONDARY_CLUSTER
 #define MULTI_CLUSTER_SETTINGS                              \
-    "init_cluster1_image=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_1_jump_addr} /vmlinuz-multi\0" \
-    "init_cluster2_image=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_2_jump_addr} /vmlinuz-multi\0" \
-    "init_cluster3_image=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_3_jump_addr} /vmlinuz-multi\0" \
-    "init_cluster1_dtb=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_1_dtb_addr} /dtb/ambarella/cluster1.dtb\0" \
-    "init_cluster3_dtb=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_3_dtb_addr} /dtb/ambarella/cluster3.dtb\0" \
-    "init_cluster2_dtb=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_2_dtb_addr} /dtb/ambarella/cluster2.dtb\0"
+    "init_cluster1_image=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_1_jump_addr} /multi-cluster/vmlinuz-multi\0" \
+    "init_cluster2_image=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_2_jump_addr} /multi-cluster/vmlinuz-multi\0" \
+    "init_cluster3_image=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_3_jump_addr} /multi-cluster/vmlinuz-multi\0" \
+    "init_cluster1_dtb=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_1_dtb_addr} /multi-cluster/dtb/ambarella/cluster1.dtb\0" \
+    "init_cluster3_dtb=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_3_dtb_addr} /multi-cluster/dtb/ambarella/cluster3.dtb\0" \
+    "init_cluster2_dtb=ext4load mmc ${emmc_dev_num}:${emmc_boot_part} ${cluster_2_dtb_addr} /multi-cluster/dtb/ambarella/cluster2.dtb\0"
 #else
 #define MULTI_CLUSTER_SETTINGS
 #endif

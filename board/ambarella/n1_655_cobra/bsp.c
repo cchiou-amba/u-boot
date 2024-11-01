205f2fe731596840572b184a8fa4b0c4f73ca694
Ian Ding <ssding@ambarella.com>
2024-11-01 17:54:35 +0800

    cobra: support sd card(mmc dev 1; fatls mmc 1)
    Change-Id: I583b65fc42136466a13404b6377fc1db34600797

--- board/ambarella/n1_655_cobra/bsp.c
+++ board/ambarella/n1_655_cobra/bsp.c
@@ -5,6 +5,7 @@
 
 #include <env.h>
 #include <dm.h>
+#include <asm/gpio.h>
 #include <asm/arch/misc.h>
 
 int dram_init(void)
@@ -23,7 +24,11 @@ int dram_init(void)
  */
 int board_init(void)
 {
-	printf("...\n");
+	/* SDMMC1 */
+	gpio_request(102, "sdmmc1_pwr");
+	gpio_request(103, "sdmmc1_v18");
+	gpio_direction_output(102, 1);
+	gpio_direction_output(103, 1);
 
 	return 0;
 }
